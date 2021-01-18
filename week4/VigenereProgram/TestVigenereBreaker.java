f8246e045a1f3795fa2ef4c0c096eed71657ea92
achromepunk <piquerojoan@outlook.es>
2021-01-18 23:06:57 +0100

    week 4 break vigenere cipher with key length known and languaje known

---
+++ week4/VigenereProgram/TestVigenereBreaker.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class TestVigenereBreaker here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import edu.duke.*;
+
+public class TestVigenereBreaker {
+    
+    public void testSlice(){
+        
+        VigenereBreaker vb = new VigenereBreaker();
+        
+        System.out.println(vb.sliceString("abcdefghijklm", 4, 5));
+        
+    }
+
+    public void testTryKeyLength(){
+        VigenereBreaker vb = new VigenereBreaker();
+        FileResource fr = new FileResource();
+        
+        String encrypted = fr.asString();
+        String key = "flute";
+        
+        int[] result = vb.tryKeyLength(encrypted, key.length(), 'e');
+        
+        for (int i = 0; i < key.length(); i++){
+            System.out.println("->" + result[i]);
+        }
+    }
+    
+    public void testBreakVigenere(){
+        VigenereBreaker vb = new VigenereBreaker();
+        vb.breakVigenere();
+    }
+    
+}
--- week4/VigenereProgram/VigenereBreaker.java
+++ week4/VigenereProgram/VigenereBreaker.java
@@ -3,18 +3,47 @@ import edu.duke.*;
 
 public class VigenereBreaker {
     public String sliceString(String message, int whichSlice, int totalSlices) {
-        //REPLACE WITH YOUR CODE
-        return "WRITE ME!";
+        //REPLACEd WITH YOUR CODE
+        StringBuilder fragment = new StringBuilder();
+        
+        for(int i = whichSlice; i < message.length(); i += totalSlices){
+            fragment.append(message.charAt(i));
+        }
+        
+        String result = fragment.toString();
+        return result;
     }
 
     public int[] tryKeyLength(String encrypted, int klength, char mostCommon) {
         int[] key = new int[klength];
         //WRITE YOUR CODE HERE
+        
+        //athens_keyflute.txt; “flute”; {5, 11, 20, 19, 4}.
+        
+        
+        for(int i = 0; i < klength; i++){
+            String fragment = sliceString(encrypted, i, klength);
+            CaesarCracker cb = new CaesarCracker(mostCommon);
+            key[i] = cb.getKey(fragment);
+        }
+        
         return key;
     }
 
     public void breakVigenere () {
         //WRITE YOUR CODE HERE
+        char mostCommon = 'e';
+        
+        FileResource fr = new FileResource();
+        String encrypted = fr.asString();
+        
+        //in this problem we know key length
+        int[] key = tryKeyLength(encrypted,5, mostCommon );
+        
+        VigenereCipher vc = new VigenereCipher(key);
+        System.out.println(vc.decrypt(encrypted));
+        
+        
     }
     
 }
